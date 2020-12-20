a5e74a181463e1858b1d391f00172bacf320df01
ataberkgrl <ataberkgrl@gmail.com>
2020-12-20 20:32:25 +0300

    api key rmw

--- import.py
+++ import.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 import csv
 import os
+import database_api_configs
 
-engine = create_engine("postgres://ybzcrpuaoapnha:9e33420befd8bc3c771cb0a6913d470ce26ffca1d66dfae68dce5ba5b7f972dc@ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/d9pcstm2bohlos")
+engine = create_engine(database_api_configs.database_url)
 
 with engine.connect() as connection:
     connection.execute("CREATE TABLE IF NOT EXISTS books (isbn TEXT NOT NULL, title TEXT NOT NULL, author TEXT NOT NULL, year TEXT NOT NULL)")
